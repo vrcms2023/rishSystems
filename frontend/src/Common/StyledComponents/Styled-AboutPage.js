85886c93483a70114d9c167f7e5845fae55238a7
unknown <prasadnti@gmail.com>
2024-01-07 10:11:32 +0530

    About Page StyledComponent Changes

---
+++ frontend/src/Common/StyledComponents/Styled-AboutPage.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const AboutPageStyled = styled.div`
+    .pageTitle {
+        color: ${({ theme }) => theme.pageTitleColor};
+    }
+
+    .title {
+        color: ${({ theme }) => theme.aboutPageTitleColor};
+    }
+    .aboutPage {
+        ul {
+            padding: 0;
+            margin: 25px 0;
+            list-style: none;
+
+            li {
+                border-bottom: 1px solid ${({ theme }) => theme.lightgray};
+                padding: 10px;
+                // background-color: ${({ theme }) => theme.verylightgray};
+            }
+
+            li:first-child {
+                // border-top-left-radius: 5px;
+                // border-top-right-radius: 5px;
+            }
+
+            li:last-child {
+                border: 0;
+            // border-bottom-left-radius: 5px;
+            // border-bottom-right-radius: 5px;
+            }
+          }
+        
+        hr:last-child {
+            display: none;
+        }
+    }
+`
--- frontend/src/Frontend/Pages/About.jsx
+++ frontend/src/Frontend/Pages/About.jsx
@@ -1,32 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Title from "../../Common/Title";
 import { toast } from "react-toastify";
+import { confirmAlert } from "react-confirm-alert";
+
+// Components
+import Title from "../../Common/Title";
 import BriefIntroFrontend from "../../Common/BriefIntro";
-import ImageInputsForm from "../../Admin/Components/forms/ImgTitleIntoForm";
-import AboutImageInputsForm from "../../Admin/Components/forms/aboutusImgTitleIntoForm";
-import AdminBriefIntro from "../../Admin/Components/BriefIntro/index";
 import EditIcon from "../../Common/AdminEditIcon";
 import ModelBg from "../../Common/ModelBg";
+import Banner from "../../Common/Banner";
+import DeleteDialog from "../../Common/DeleteDialog";
+import { useAdminLoginStatus } from "../../Common/customhook/useAdminLoginStatus";
+
 import AddEditAdminNews from "../../Admin/Components/News";
+import ImageInputsForm from "../../Admin/Components/forms/ImgTitleIntoForm";
+import AdminBriefIntro from "../../Admin/Components/BriefIntro/index";
+
 import { removeActiveClass } from "../../util/ulrUtil";
 import {
   getFormDynamicFields,
   getAboutUSSectionFields,
   imageDimensionsJson,
 } from "../../util/dynamicFormFields";
-import { useAdminLoginStatus } from "../../Common/customhook/useAdminLoginStatus";
-
-import Banner from "../../Common/Banner";
-import AboutSection from "../Components/AboutSection";
-import { axiosClientServiceApi, axiosServiceApi } from "../../util/axiosUtil";
 import { getImagePath } from "../../util/commonUtil";
-import { confirmAlert } from "react-confirm-alert";
-import DeleteDialog from "../../Common/DeleteDialog";
 import { sortByUpdatedDate } from "../../util/dataFormatUtil";
-import moment from "moment";
+import { axiosClientServiceApi, axiosServiceApi } from "../../util/axiosUtil";
 
-import "./About.css";
+// CSS
+import { AboutPageStyled } from "../../Common/StyledComponents/Styled-AboutPage";
 
 const About = () => {
   const editComponentObj = {
@@ -163,141 +164,143 @@ const About = () => {
         ""
       )}
 
-      <div className="container-fluid container-lg my-md-5 ">
-        <div className="row">
-          <div className="col-md-6 fs-3 mt-4 mt-md-0">
-            <Title title="About Us" cssClass="fs-1" />
-          </div>
-          {isAdmin ? (
-            <div className="col-md-6">
-              <div className="d-flex justify-content-end align-items-center mb-3">
-                <span className="fw-bold me-2">Add content </span>
-                <button
-                  type="submit"
-                  className="btn btn-primary px-3"
-                  onClick={() => editHandler("addSection", true)}
-                >
-                  {" "}
-                  <i className="fa fa-plus" aria-hidden="true"></i>
-                </button>
+      <AboutPageStyled>
+        <div className="container-fluid container-lg my-md-5 ">
+          <div className="row">
+            <div className="col-md-6 fs-3 mt-4 mt-md-0">
+              <Title title="About Us" cssClass="fs-1 pageTitle" />
+            </div>
+            {isAdmin ? (
+              <div className="col-md-6">
+                <div className="d-flex justify-content-end align-items-center mb-3">
+                  <span className="fw-bold me-2">Add content </span>
+                  <button
+                    type="submit"
+                    className="btn btn-primary px-3"
+                    onClick={() => editHandler("addSection", true)}
+                  >
+                    {" "}
+                    <i className="fa fa-plus" aria-hidden="true"></i>
+                  </button>
+                </div>
               </div>
+            ) : (
+              ""
+            )}
+          </div>
+
+          {componentEdit.editSection || componentEdit.addSection ? (
+            <div className="adminEditTestmonial">
+              <AddEditAdminNews
+                editHandler={editHandler}
+                category="about"
+                editCarousel={editCarousel}
+                setEditCarousel={setEditCarousel}
+                componentType={`${
+                  componentEdit.editSection ? "editSection" : "addSection"
+                }`}
+                imageGetURL="aboutus/clientAboutus/"
+                imagePostURL="aboutus/createAboutus/"
+                imageUpdateURL="aboutus/updateAboutus/"
+                imageDeleteURL="aboutus/updateAboutus/"
+                imageLabel="Add About us Banner"
+                showDescription={false}
+                showExtraFormFields={getAboutUSSectionFields()}
+                dimensions={imageDimensionsJson("aboutus")}
+              />
             </div>
           ) : (
             ""
           )}
-        </div>
 
-        {componentEdit.editSection || componentEdit.addSection ? (
-          <div className="adminEditTestmonial">
-            <AddEditAdminNews
-              editHandler={editHandler}
-              category="about"
-              editCarousel={editCarousel}
-              setEditCarousel={setEditCarousel}
-              componentType={`${
-                componentEdit.editSection ? "editSection" : "addSection"
-              }`}
-              imageGetURL="aboutus/clientAboutus/"
-              imagePostURL="aboutus/createAboutus/"
-              imageUpdateURL="aboutus/updateAboutus/"
-              imageDeleteURL="aboutus/updateAboutus/"
-              imageLabel="Add About us Banner"
-              showDescription={false}
-              showExtraFormFields={getAboutUSSectionFields()}
-              dimensions={imageDimensionsJson("aboutus")}
-            />
-          </div>
-        ) : (
-          ""
-        )}
-
-        <div className="row aboutPage">
-          {aboutList.length > 0 ? (
-            aboutList.map((item, index) => (
-              <>
-                <div
-                  key={item.id}
-                  className={`row mb-2 ${
-                    isAdmin
-                      ? "border border-warning mb-3 position-relative"
-                      : ""
-                  } ${index % 2 === 0 ? "normalCSS" : "flipCSS"}`}
-                >
-                  {isAdmin ? (
-                    <>
-                      <EditIcon
-                        editHandler={() =>
-                          editHandler("editSection", true, item)
-                        }
-                      />
-                      <Link
-                        className="deleteSection"
-                        onClick={() => deleteAboutSection(item)}
-                      >
-                        <i
-                          className="fa fa-trash-o text-danger fs-4"
-                          aria-hidden="true"
-                        ></i>
-                      </Link>
-                    </>
-                  ) : (
-                    ""
-                  )}
-                  <div className="col-12 col-lg-7 p-3 p-md-4 py-md-4 d-flex justify-content-center align-items-start flex-column">
-                    {item.aboutus_title ? (
-                      <Title
-                        title={item.aboutus_title}
-                        cssClass="fs-1 fw-bold mb-1"
-                      />
+          <div className="row aboutPage">
+            {aboutList.length > 0 ? (
+              aboutList.map((item, index) => (
+                <>
+                  <div
+                    key={item.id}
+                    className={`row mb-2 ${
+                      isAdmin
+                        ? "border border-warning mb-3 position-relative"
+                        : ""
+                    } ${index % 2 === 0 ? "normalCSS" : "flipCSS"}`}
+                  >
+                    {isAdmin ? (
+                      <>
+                        <EditIcon
+                          editHandler={() =>
+                            editHandler("editSection", true, item)
+                          }
+                        />
+                        <Link
+                          className="deleteSection"
+                          onClick={() => deleteAboutSection(item)}
+                        >
+                          <i
+                            className="fa fa-trash-o text-danger fs-4"
+                            aria-hidden="true"
+                          ></i>
+                        </Link>
+                      </>
                     ) : (
                       ""
                     )}
+                    <div className="col-12 col-lg-7 p-3 p-md-4 py-md-4 d-flex justify-content-center align-items-start flex-column">
+                      {item.aboutus_title ? (
+                        <Title
+                          title={item.aboutus_title}
+                          cssClass="fs-2 mb-1 title"
+                        />
+                      ) : (
+                        ""
+                      )}
 
-                    {item.aboutus_sub_title ? (
-                      <Title
-                        title={item.aboutus_sub_title}
-                        cssClass="fs-5 text-secondary mb-2"
-                      />
-                    ) : (
-                      ""
-                    )}
-                    {/* <p>{moment(item.created_at).format('DD-MM-YYYY hh:mm:ss')}</p> */}
-                    {item.aboutus_sub_title ? (
-                      <Title
-                        title={item.aboutus_sub_title}
-                        cssClass="fs-5 text-secondary mb-2"
-                      />
-                    ) : (
-                      ""
-                    )}
+                      {item.aboutus_sub_title ? (
+                        <Title
+                          title={item.aboutus_sub_title}
+                          cssClass="fs-5 text-secondary mb-2"
+                        />
+                      ) : (
+                        ""
+                      )}
+                      {/* <p>{moment(item.created_at).format('DD-MM-YYYY hh:mm:ss')}</p> */}
+                      {item.aboutus_sub_title ? (
+                        <Title
+                          title={item.aboutus_sub_title}
+                          cssClass="fs-5 text-secondary mb-2"
+                        />
+                      ) : (
+                        ""
+                      )}
 
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: item.aboutus_description,
-                      }}
-                    />
-                  </div>
-
-                  <div className="col-lg-5 d-none d-lg-block h-100">
-                    <div className="h-100 p-3 p-md-5 py-md-4 d-flex flex-column justify-content-center align-items-center reset ">
-                      <img
-                        src={getImagePath(item.path)}
-                        alt=""
-                        className="img-fluid"
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: item.aboutus_description,
+                        }}
                       />
                     </div>
+
+                    <div className="col-lg-5 d-none d-lg-block h-100">
+                      <div className="h-100 p-3 p-md-5 py-md-4 d-flex flex-column justify-content-center align-items-center reset ">
+                        <img
+                          src={getImagePath(item.path)}
+                          alt=""
+                          className="img-fluid"
+                        />
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <hr className="border-secondary" />
-              </>
-            ))
-          ) : (
-            <p className="text-center text-muted py-5">
-              Please add page contents...
-            </p>
-          )}
+                  <hr className="border-secondary" />
+                </>
+              ))
+            ) : (
+              <p className="text-center text-muted py-5">
+                Please add page contents...
+              </p>
+            )}
+          </div>
         </div>
-      </div>
+      </AboutPageStyled>
 
       {show && <ModelBg />}
     </>
