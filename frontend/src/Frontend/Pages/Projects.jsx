daa86cc92dcb88099454e2da2e1bbc964f1eed07
Ramakrishna <jetti@Ramakrishna.local>
2024-01-11 11:36:51 +0530

    remove menu active class

--- frontend/src/Frontend/Pages/Projects.jsx
+++ frontend/src/Frontend/Pages/Projects.jsx
@@ -14,6 +14,7 @@ import {
 } from "../../util/dynamicFormFields";
 import "./Projects.css";
 import ImageInputsForm from "../../Admin/Components/forms/ImgTitleIntoForm";
+import { removeActiveClass } from "../../util/ulrUtil";
 
 const Projects = () => {
   const editComponentObj = {
@@ -50,6 +51,10 @@ const Projects = () => {
 
   useEffect(() => {
     window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+  removeActiveClass()
   }, []);
 
   const editHandler = (name, value) => {
